02f1bd1f67234892245c70e0a1bac3ba63fc84df
guiluizmaia <68974994+guiluizmaia@users.noreply.github.com>
2021-02-17 21:46:37 -0300

    Add files via upload

--- src/controllers/UserController.js
+++ src/controllers/UserController.js
@@ -1,11 +1,36 @@
 const User = require('../models/User');
+const bcrypt = require('bcrypt');
 
 module.exports = {
+    async index(req, res){
+        const { user, password} = req.body;
+        
+        const retorno = await User.findOne({where: {user: user}})
+
+        if(!retorno){
+            return res.status(400).json({ error: 'User not found' });
+        }
+
+        if(bcrypt.compareSync(password, retorno.password)){
+            return res.json({"logado": retorno.user});
+        }
+
+        return res.status(400).json({ error: 'User not found' });
+    },       
+
     async store(req, res){
-        const { user, password } = req.body;
+        const { user } = req.body;
 
-        const userpront = await User.create({ user, password });
+        const userRepetido = await User.findOne({where: {user: user}})
+        
+        if(!userRepetido){
+            const password = bcrypt.hashSync(req.body.password, 10);
 
-        return res.json(userpront);
+            const userpront = await User.create({ user, password });
+
+            return res.json(userpront);
+        }
+
+        return res.json({ error: 'Usuário ja cadastrado'})
     }
 };
--- src/routes.js
+++ src/routes.js
@@ -4,6 +4,8 @@ const UserController = require('./controllers/UserController');
 
 const routes = express.Router();
 
+
+routes.post('/login', UserController.index);
 routes.post('/createUser', UserController.store);
 
 module.exports = routes;
